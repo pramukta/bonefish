e5ba4bce6641a99714d4ba3e278c68e12b020b7d
Pramukta Kumar <pramukta.kumar@gmail.com>
2016-01-07 13:57:27 -0600

    remove router if it doesn't have any sessions and we are in dynamic mode

--- src/bonefish/websocket/websocket_server_impl.cpp
+++ src/bonefish/websocket/websocket_server_impl.cpp
@@ -151,6 +151,9 @@ void websocket_server_impl::on_close(websocketpp::connection_hdl handle)
                 m_routers->get_router(connection->get_realm());
         if (router) {
             router->detach_session(connection->get_session_id());
+            if (!router->has_sessions() && m_routers->is_dynamic()) {
+                m_routers->remove_router(router->get_realm())
+            }
         }
     }
 }
@@ -165,6 +168,9 @@ void websocket_server_impl::on_fail(websocketpp::connection_hdl handle)
                 m_routers->get_router(connection->get_realm());
         if (router) {
             router->detach_session(connection->get_session_id());
+            if (!router->has_sessions() && m_routers->is_dynamic()) {
+                m_routers->remove_router(router->get_realm())
+            }
         }
     }
 }
