4d1ca2c0d72dc5c8c970afc3c5dd1569e5f28d45
Pramukta Kumar <pramukta.kumar@gmail.com>
2016-01-07 11:23:24 -0600

    dynamic realms integration example

--- examples/integration.cpp
+++ examples/integration.cpp
@@ -21,6 +21,7 @@
 #include <bonefish/router/wamp_routers.hpp>
 #include <bonefish/serialization/wamp_serializers.hpp>
 #include <bonefish/serialization/msgpack_serializer.hpp>
+#include <bonefish/serialization/json_serializer.hpp>
 #include <bonefish/trace/trace.hpp>
 #include <bonefish/websocket/websocket_server.hpp>
 
@@ -43,37 +44,21 @@ int main(int argc, char** argv)
     boost::asio::io_service io_service;
 
     std::shared_ptr<bonefish::wamp_routers> routers =
-            std::make_shared<bonefish::wamp_routers>();
+            std::make_shared<bonefish::wamp_routers>(io_service);
 
-    std::shared_ptr<bonefish::wamp_router> router =
-            std::make_shared<bonefish::wamp_router>(io_service, "default");
+    // std::shared_ptr<bonefish::wamp_router> router =
+    //         std::make_shared<bonefish::wamp_router>(io_service, "default");
 
-    routers->add_router(router);
+    // routers->add_router(router);
 
     std::shared_ptr<bonefish::wamp_serializers> serializers =
             std::make_shared<bonefish::wamp_serializers>();
     serializers->add_serializer(std::make_shared<bonefish::msgpack_serializer>());
-
-    std::shared_ptr<bonefish::native_server> native_server =
-            std::make_shared<bonefish::native_server>(io_service, routers);
-    native_server->start();
-
-    std::shared_ptr<bonefish::rawsocket_server> rawsocket_server =
-            std::make_shared<bonefish::rawsocket_server>(routers, serializers);
-    std::shared_ptr<bonefish::uds_listener> uds_listener =
-            std::make_shared<bonefish::uds_listener>(io_service, "/tmp/bonefish.sock");
-    std::shared_ptr<bonefish::tcp_listener> tcp_listener =
-            std::make_shared<bonefish::tcp_listener>(io_service, boost::asio::ip::address(), 8888);
-
-    rawsocket_server->attach_listener(
-            std::static_pointer_cast<bonefish::rawsocket_listener>(uds_listener));
-    rawsocket_server->attach_listener(
-            std::static_pointer_cast<bonefish::rawsocket_listener>(tcp_listener));
-    rawsocket_server->start();
+    serializers->add_serializer(std::make_shared<bonefish::json_serializer>());
 
     std::shared_ptr<bonefish::websocket_server> websocket_server =
             std::make_shared<bonefish::websocket_server>(io_service, routers, serializers);
-    websocket_server->start(boost::asio::ip::address(), 9999);
+    websocket_server->start(boost::asio::ip::address(), 8080);
 
     io_service.run();
 
